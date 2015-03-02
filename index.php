5af0ec39c40d1499cf521a076006b42816501877
MTudaj <gtudaj@gmail.com>
2015-03-02 20:52:47 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,196 @@
+<?php
+require_once('funkcje.php');
+session_start();
+$a = session_id();
+include ('configg.php');
+
+mysql_connect($_CONFIG['MySQL']['host'], $_CONFIG['MySQL']['user'], $_CONFIG['MySQL']['pass'])
+or die("Nie udało się połączyć");
+mysql_select_db($_CONFIG['MySQL']['base']);
+
+main();
+?>
+<p align="left"><b>Lista dostępnych filmów: </b></p>
+<hr>
+<p align="center">
+<?php
+		/////////////////////////////////  DOSTĘPNE FILMY ///////////////////////////////////////////////
+
+mysql_query ("SET NAMES latin2");
+
+$zapytanie_4 = mysql_query("SELECT * FROM film where rezerwacja=0")
+ or die(mysql_error());
+
+ mysql_query ("SET NAMES latin2");
+ echo'<table border="1"   bgcolor="white" cellpadding="2" cellspacing="0" style="border-collapse: collapse" bordercolor="#111111" id="AutoNumber1">';
+echo "
+<td bgcolor='#68A898'><center><b><center>TYTYUŁ</td></center></b></b></td>
+<td bgcolor='#68A898'><center><b><center>GATUNEK</td></center></b></b></td>
+<td bgcolor='#68A898'><center><b><center>OKŁADKA</center></b></td>
+<td bgcolor='#68A898'><center><b><center>OPIS</center></b></td>
+<td bgcolor='#68A898'><center><b><center>AKTORZY</center></b></td>
+<td bgcolor='#68A898'><center><b><center>CENA</center></b></td></tr>";
+while ($wierszz=mysql_fetch_row($zapytanie_4))
+{
+echo"<tr class='yellow' width='15%'><td><center>$wierszz[1]</td>";
+echo"<td><center>$wierszz[2]</td>";	
+	  ?>	
+<td><center><a href="film_detale.php?id=<?php echo"$wierszz[0]";?>.php
+"><span style="text-decoration: none"><font color="#669999"><img border="0" src="filmy_covers/<?php echo"$wierszz[3]";?>"
+width="100" height="100"></font></span></a>
+</td>
+<?php
+echo"<td><center>$wierszz[4]</td>";
+echo"<td><center>$wierszz[9]</td>";
+echo"<td><center>$wierszz[5]</td>";
+  
+	 }
+	echo'</table>';
+?>
+<?php
+///////////////////////////////////////////////////////// TOP WYPOŻYCZANE //////////////////////////////////////////////
+
+?>
+<br><br>
+<b><p align="left">Najczęściej wypożyczane: </b></p>
+<p align="center">
+<hr>
+
+<?php
+$zapytanie_4 = mysql_query("SELECT * FROM film order by wypozyczen_ilosc DESC")
+ or die(mysql_error());
+
+ mysql_query ("SET NAMES latin2");
+ echo'<table border="1"   bgcolor="white" cellpadding="2" cellspacing="0" style="border-collapse: collapse" bordercolor="#111111" id="AutoNumber1">';
+echo "
+<td bgcolor='#68A898'><center><b><center>TYTYUŁ</td></center></b></b></td>
+<td bgcolor='#68A898'><center><b><center>GATUNEK</td></center></b></b></td>
+<td bgcolor='#68A898'><center><b><center>OKŁADKA</center></b></td>
+<td bgcolor='#68A898'><center><b><center>OPIS</center></b></td>
+<td bgcolor='#68A898'><center><b><center>AKTORZY</center></b></td>
+<td bgcolor='#68A898'><center><b><center>CENA</center></b></td>
+<td bgcolor='#68A898'><center><b><center>ILOŚĆ WYPOŻYCZEŃ</center></b></td>";
+
+echo "</tr>";
+while ($wierszz=mysql_fetch_row($zapytanie_4))
+{
+  echo"<tr class='yellow' width='15%'><td><center>$wierszz[1]</td>";
+  echo"<td><center>$wierszz[2]</td>";	
+	  ?>	
+
+
+<td><center><a href="film_detale.php?id=<?php echo"$wierszz[0]";?>.php"><span style="text-decoration: none"><font color="#669999">
+		<img border="0" src="filmy_covers/
+		<?php echo"$wierszz[3]"; ?>"
+	
+		
+		width="160" height="160"></font></span></a>
+</td>
+<?php
+echo"<td><center>$wierszz[4]</td>";
+echo"<td><center>$wierszz[9]</td>";
+echo"<td><center>$wierszz[5]</td>";
+echo"<td><center>$wierszz[10]</td>";
+  
+	 }
+	echo'</table>';
+
+////////////////////////////////////////////////////////////////////////////////////
+?>
+<br>
+<br>
+<p align="left"><b>Lista najczęściej recenzowanych:</b></p>
+<hr>
+<?php
+
+mysql_connect($_CONFIG['MySQL']['host'], $_CONFIG['MySQL']['user'], $_CONFIG['MySQL']['pass'])
+or die("Nie udało się poł&#177;czyć");
+mysql_select_db($_CONFIG['MySQL']['base']);
+mysql_query ("SET NAMES latin2");
+
+
+$zapytanie_4 = mysql_query("SELECT DISTINCT * FROM recenzja order by id DESC")
+ or die(mysql_error());
+
+ mysql_query ("SET NAMES latin2");
+ echo'<table border="1"   bgcolor="white" cellpadding="2" cellspacing="0" style="border-collapse: collapse" bordercolor="#111111" id="AutoNumber1">';
+echo "
+<td bgcolor='#68A898'><center><b><center>TYTUŁ</td></center></b></b></td>
+<td bgcolor='#68A898'><center><b><center>ILOŚĆ RECENZJI</td></center></b></b></td>
+<td bgcolor='#68A898'><center><b><center>ZOBACZ RECENZJĘ</td></center></b></b></td></tr>";
+while ($wierszz=mysql_fetch_row($zapytanie_4))
+{
+	
+  echo"<tr class='yellow' width='15%'><td><center>";
+	?>
+	<?php
+	$zapytanie_14 = mysql_query("SELECT DISTINCT tytul, id FROM film where id='$wierszz[1]'");
+	    // wyświetlany wyniki zapytania 
+    while($rek = mysql_fetch_array($zapytanie_14)) { 
+        echo $rek['tytul']."<br />"; 
+    } 
+	?>
+	<?php
+	echo"</td><td><center>";
+?>
+<?php
+$query8 = mysql_query('SELECT DISTINCT count(id) AS ile FROM recenzja WHERE id_filmu LIKE \''.$wierszz[1].'\'');
+    $wynik8= mysql_fetch_array( $query8 );
+    echo ''.$wynik8['ile'].' ';
+
+?>
+<?php
+echo"</td>";
+echo"<td><center><a href='film_detale.php?id=$wierszz[1]'>zobacz recenzję użytkowników</a></td>";
+  
+	 }
+	echo'</table>';
+
+////////////////////////////////////////////////////////////////////////////////////
+?>
+<p align="left">&nbsp;</p>
+<p align="left"><b>GATUNKI:</b></p>
+<hr>
+<?php
+mysql_connect($_CONFIG['MySQL']['host'], $_CONFIG['MySQL']['user'], $_CONFIG['MySQL']['pass'])
+	or die("Nie udało się poł&#177;czyć");
+mysql_select_db($_CONFIG['MySQL']['base']);
+ mysql_query ("SET NAMES latin2");
+
+$zapytanie_4 = mysql_query("SELECT * FROM gatunek")
+ or die(mysql_error());
+
+ mysql_query ("SET NAMES latin2");
+ echo'<table border="1"   bgcolor="white" cellpadding="2" cellspacing="0" style="border-collapse: collapse" bordercolor="#111111" id="AutoNumber1">';
+echo "
+<td bgcolor='#68A898'><center><b><center>GATUNEK</td></center></b></b></td>
+<td bgcolor='#68A898'><center><b><center>ILOŚĆ FILMÓW</td></center></b></b></td>
+<td bgcolor='#68A898'><center><b><center>ZOBACZ FILMY</td></center></b></b></td></tr>";
+while ($wierszz=mysql_fetch_row($zapytanie_4))
+{
+	
+  echo"<tr class='yellow' width='15%'><td><center>$wierszz[1]</td><td><center>";
+?>
+<?php
+$query8 = mysql_query('SELECT count(id) AS ile FROM film WHERE gatunek LIKE \''.$wierszz[1].'\'');
+    $wynik8= mysql_fetch_array( $query8 );
+    echo ''.$wynik8['ile'].' ';
+
+?>
+<?php
+echo"</td>";
+echo"<td><center><a href='gatunki.php?gatunek=$wierszz[1]'>zobacz filmy z tej kategorii</a></td>";
+  
+	 }
+	echo'</table>';
+?>
+</html>
+
+<?php
+stopka();
+?>
+
+
+
+
+
